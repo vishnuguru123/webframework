b170f7a401d040037bf657a14720b17b5fe795f2
vishnuguru123 <119098678+vishnuguru123@users.noreply.github.com>
2024-04-12 19:16:11 +0530

    Update SecondController.java

--- Webframeworks/day1/day1q2/src/main/java/com/example/day1q2/Controller2/SecondController.java
+++ Webframeworks/day1/day1q2/src/main/java/com/example/day1q2/Controller2/SecondController.java
@@ -2,7 +2,15 @@ package com.example.day1q2.Controller2;
 
 import org.springframework.web.bind.annotation.RestController;
 
-public class SecondController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 @RestController
-
+public class SecondController {
+    @GetMapping("/studentName")
+    public String welcomeStudent(@RequestParam("studentName") String studentName) {
+        return "Welcome " + studentName + "!";
+    }
 }
+
+
+
