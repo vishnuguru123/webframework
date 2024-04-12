57028d9686033f952994b91207d8a28fae3441ed
vishnuguru123 <119098678+vishnuguru123@users.noreply.github.com>
2024-04-12 19:22:38 +0530

    Create Student.java

---
+++ Webframeworks/day1/day1q5/src/main/java/com/example/day1q5/Student.java
@@ -0,0 +1,21 @@
+package com.example.day1q5.model;
+
+public class student {
+    
+
+    private String studentName;
+    private String message;
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message){
+        this.message=message;
+    }
+}
+
